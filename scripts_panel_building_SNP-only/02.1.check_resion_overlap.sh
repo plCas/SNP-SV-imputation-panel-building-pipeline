9ddf2908f71e2babca427b41453f85fd1d652393
plCas <52269575+plCas@users.noreply.github.com>
2025-03-03 15:58:09 -0500

    Delete scripts_panel_building_SNP-only directory

--- scripts_panel_building_SNP-only/02.1.check_resion_overlap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CHR=$1
-OUT=/qnap-wlee/wanpingleelab/chengp/36K_panel_build/test_region_overlap
-mkdir -p $OUT
-VCF=/qnap-wlee/wanpingleelab/chengp/36K_panel_build/vcfs/filtered_bi/chr$CHR.bi.vcf.gz
-
-bcftools query -f "%POS\n" $VCF  > $OUT/chr$CHR.pos.txt
