55f02cdd0d146b88460cd65aad5261179e74a868
plCas <52269575+plCas@users.noreply.github.com>
2025-03-03 16:12:09 -0500

    Update 02.bcftools_filter_wgs.sh

--- WGS validation/SNV_indels/02.bcftools_filter_wgs.sh
+++ WGS validation/SNV_indels/02.bcftools_filter_wgs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 ETH=$1 # ADGC_NHW, ADGC_AA, ADGC_Asian, ADGC_Hispanic
-
 CHR=$2
-
-VCF=/../$ETH/chr$CHR.vcf.gz
+VCF=/../chr$CHR.vcf.gz
 
 SAMPLES=/../WGS_validation/SNV_Indel/files/${ETH}_sample_list.txt
 OUT=/../WGS_validation/SNV_Indel/vcfs/wgs/$ETH
