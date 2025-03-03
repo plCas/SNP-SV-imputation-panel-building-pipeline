ebe8598571095fb7708675ab0f431856b7da81c6
plCas <52269575+plCas@users.noreply.github.com>
2025-03-03 16:15:22 -0500

    Update 03.aggRsqure.sh

--- WGS validation/SNV_indels/03.aggRsqure.sh
+++ WGS validation/SNV_indels/03.aggRsqure.sh
@@ -7,6 +7,6 @@ OUT=$3
 /../aggRSquare/release-build/aggRSquare \
     -v $TRUTH_VCF \
     -i $TEST_VCF \
-	--bins /../bin_used.txt \
-	--imputationFormat GT \
+    --bins /../bin_used.txt \
+    --imputationFormat GT \
     -o $OUT
